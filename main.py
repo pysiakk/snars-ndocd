b9db9755d905c1fbc1dd0faab460eb80c11e734f
Karol Pysiak <karol.pysiak.13@gmail.com>
2022-01-04 10:48:39 +0100

    First tries NDOCD

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from NDOCD import NDOCD
 import Community
 import numpy as np
+from scipy import sparse
 
-d1k2 = np.genfromtxt("Communities/D1-K=2.csv", delimiter=",")
-ndocd = NDOCD(d1k2)
+d1k2 = sparse.csr_matrix(np.genfromtxt("Communities/D1-UNC.csv", delimiter=","))
+ndocd = NDOCD(d1k2, modification=True, modification_number=100, modification_type="number")
 ndocd.JS_threshold = 0.3
 ndocd.MD_threshold = 0.3
 coms = ndocd.find_all_communities()
-print(coms)
+for com in coms:
+    print(np.sum(com.todense()))
