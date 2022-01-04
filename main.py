4efb854b43d8bb75c170571087d0996be35e53f0
Karol Pysiak <karol.pysiak.13@gmail.com>
2022-01-04 10:31:25 +0100

    And... you failed

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from NDOCD import NDOCD
 import Community
+import numpy as np
 
-ndocd = NDOCD()
+d1k2 = np.genfromtxt("Communities/D1-K=2.csv", delimiter=",")
+ndocd = NDOCD(d1k2)
+ndocd.JS_threshold = 0.3
+ndocd.MD_threshold = 0.3
+coms = ndocd.find_all_communities()
+print(coms)
